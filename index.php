ce96d03d0c8b22e26a5952cb3f746aa8e8ba6e86
Sushma Yadav <sushma.developer@appmanufact.com>
2022-10-01 22:17:47 +0530

    merge command

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <title>Git Tutorial</title>
     </head>
     <body>
-        <h1>Git merge Tutorial</h1>
+        <h1>Git merge Tutorial - merge check</h1>
         <p>New file created - all merged diles</p>
     </body>
 </html>
