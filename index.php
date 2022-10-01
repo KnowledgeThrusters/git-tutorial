bc648e6b3c1fc18a34267042786b18f7c57714ec
KnowledgeThrusters <109918619+KnowledgeThrusters@users.noreply.github.com>
2022-10-01 10:44:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <title>Git Tutorial</title>
     </head>
     <body>
-        <h1>Git add Tutorial</h1>
-        <p>New file created</p>
+        <h1>Git merge Tutorial</h1>
+        <p>New file created - all merged diles</p>
     </body>
 </html>
